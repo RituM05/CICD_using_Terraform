7786e2f2f8a9f9fcac549b4691ea2c0f013bfe91
Rituparna Maity <ritz.maity@gmail.com>
2025-04-16 16:45:50 +0530

    testing the pipeline

--- nodejsapp/app.js
+++ nodejsapp/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Hello, from Ritu");
+    res.send("Hello, from Ritu, pipeline is running successfully..");
 });
 
 app.listen(8080, ()=>{
